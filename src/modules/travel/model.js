56229eadd025cdf575a7a87e70527e9b6a0af2e8
Mensu <yxshw55@qq.com>
2018-03-09 01:20:51 +0800

    修复创建游记记录时id不存在

--- src/modules/travel/model.js
+++ src/modules/travel/model.js
@@ -127,7 +127,17 @@ SELECT travel.travel_id, user.user_id, user.nickname, travel.title,
  * @param {number} travel_id
  */
 export async function findById(travel_id) {
-  const [travel] = await findList({ travel_id }, 0);
+  const sql = `
+SELECT travel_id, user_id, title
+  FROM travel
+  WHERE travel_id = ? AND travel.is_deleted = 0
+;
+`;
+  const values = [travel_id];
+  /** @type {[Travel]} */
+  const [travel] = await query(sql, values);
+  const [travelWithRecord = { records: [] }] = await findList({ travel_id }, 0);
+  assign(travel, travelWithRecord);
   return travel;
 }
 
